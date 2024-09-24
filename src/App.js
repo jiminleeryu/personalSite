255895ff741d605d3d2b876c34af046777272d14
Jimin Ryu <jimin_ryu@brown.edu>
2024-09-24 15:19:54 -0400

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="" element={<Home />} />
         <Route path="/Home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/spotifinder" element={<Spotifinder />} />
