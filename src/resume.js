117db858d311612e7df991e445933bb60f286e42
Jimin Ryu <jimin_ryu@brown.edu>
2026-04-27 16:11:07 -0400

    fixed eslint error

--- src/resume.js
+++ src/resume.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import { useState, useEffect } from 'react';
 import './styles/resume.css';
 import './styles/global.css';
 
@@ -10,12 +7,12 @@ const Resume = () => {
         <div className="Resume">
             <header className="resume-header">
                 <div className="header-brand">
-                    <a aria-content="page" className="brand-link" href="/Home">Jimin Ryu</a>
+                    <a aria-label="page" className="brand-link" href="/Home">Jimin Ryu</a>
                 </div>
                 <nav>
                     <ul className="header-links">
                         <li>
-                            <a aria-content="page" className="link" href="/home">About</a>
+                            <a aria-label="page" className="link" href="/home">About</a>
                         </li>
                         <li>
                             <a className="link" href="/projects">Projects</a>
@@ -39,13 +36,13 @@ const Resume = () => {
             <footer className="footer">
                 <p>© 2024 Jimin Ryu</p>
                 <div className="social-icons-container-footer">
-                    <a href="https://github.com/jiminleeryu" target="_blank" aria-label="GitHub">
+                    <a href="https://github.com/jiminleeryu" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
                         <i class="fab fa-github" aria-hidden="true"></i>
                     </a>
-                    <a href="https://linkedin.com/in/jryu0" target="_blank" aria-label="LinkedIn">
+                    <a href="https://linkedin.com/in/jryu0" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                         <i class="fab fa-linkedin" aria-hidden="true"></i>
                     </a>
-                    <a href="mailto:jiminleeryu@gmail.com" target="_blank" aria-label="Email">
+                    <a href="mailto:jiminleeryu@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email">
                         <i class="fas fa-envelope" aria-hidden="true"></i>
                     </a>
                 </div>
